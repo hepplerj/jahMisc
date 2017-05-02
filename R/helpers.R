1cb60b10439db331855035f35aa4810c82078629
hepplerj <jason.heppler@gmail.com>
2017-05-02 10:08:20 -0500

    New theme.

--- R/helpers.R
+++ R/helpers.R
@@ -11,14 +11,14 @@
 #' @export
 
 geo_join <- function(spatial_data, data_frame, by_sp, by_df) {
-  
+
   spatial_data@data <- data.frame(spatial_data@data,
                                   data_frame[match(spatial_data@data[[by_sp]],
                                                    data_frame[[by_df]]), ])
-  
+
   spatial_data
-  
-  
+
+
 }
 
 #'  Quick transform of spatial objects to WGS84
@@ -56,4 +56,37 @@ quantile_labels <- function(vec, n) {
   }
   final_labs <- c(qlabs, "Data unavailable")
   final_labs
+}
+
+#' Minimal ggplot2 theme using the Roboto Condensed and Roboto Bold fonts
+#'
+#' @param base_size base font size
+#' @param ... Other arguments passed to \code{theme_minimal}
+#'
+#' @details The Roboto Condensed and Roboto Bold fonts are both Google fonts;
+#' they can be found at \url{https://fonts.google.com/specimen/Roboto+Condensed}
+#' and \url{https://fonts.google.com/specimen/Roboto}. These fonts must be
+#' installed locally on your computer for this theme to work.
+#'
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#'
+#' ggplot(mtcars, aes(wt, mpg)) +
+#'     geom_point() +
+#'     labs(title = "A Lovely Plot",
+#'          subtitle = "What can the subtitle tell us?") +
+#'     theme_roboto()
+#'}
+#'
+#' @export
+theme_roboto <- function(base_size = 11, ...) {
+  ret <- ggplot2::theme_minimal(base_family = "RobotoCondensed-Regular",
+                                base_size = base_size, ...)
+  ret$plot.title <- ggplot2::element_text(family="Roboto-Bold",
+                                          size = rel(1.2),
+                                          hjust = 0, vjust = 1,
+                                          margin = ggplot2::margin(b = base_size / 2 * 1.2)
+  )
+  ret
 }
